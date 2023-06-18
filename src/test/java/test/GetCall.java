3b12e5eb15e5495fee6cdc35759e85919a6534fa
sachdeva-japneet <japneetsachdeva1@gmail.com>
2023-06-18 11:32:08 +0530

    first commit

---
+++ src/test/java/test/GetCall.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class GetCall
+{
+    @Test
+    public void testGetCall()
+    {
+        
+    }
+}
